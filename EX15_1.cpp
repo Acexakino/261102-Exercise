6f67096511ea8f536d45251da96add4b7bb0ef79
Supadet Borisutthanarak <35340249+Acexakino@users.noreply.github.com>
2018-03-13 14:14:01 +0700

    Exercise 15

---
+++ EX15_1.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int sumNonZero(int *,int **,int **);
+
+int main(){
+	int A[] = {0,0,0,0,0,1,3,2,1,1,0,0,0,1,6,9,0,0,0,1,1,0,0};
+	int sum;
+	int *start, *end;
+	
+	sum = sumNonZero(A,&start,&end);
+	cout << "sum = " << sum << "\n";
+	cout << "start = " << start << "\n" ;
+	cout << "end = " << end << "\n";
+	cout << "length = " << end-start+1 << "\n";
+	cout <<"--------------------------------------\n";
+
+	sum = sumNonZero(end+1,&start,&end);
+	cout << "sum = " << sum << "\n";
+	cout << "start = " << start << "\n" ;
+	cout << "end = " << end << "\n";
+	cout << "length = " << end-start+1 << "\n";
+	cout <<"--------------------------------------\n";
+
+	sum = sumNonZero(end+1,&start,&end);
+	cout << "sum = " << sum << "\n";
+	cout << "start = " << start << "\n" ;
+	cout << "end = " << end << "\n";
+	cout << "length = " << end-start+1 << "\n";
+	cout <<"--------------------------------------\n";
+
+	return 0;
+}
+
+int sumNonZero(int *d,int **s,int **e){
+	int sum = 0;
+	for(int i = 0; i < 23; i++){
+		if(*(d+i) != 0){
+			sum += *(d+i);
+			if(*(d+i-1) == 0){
+				*s = (d+i);
+			} 
+			if(*(d+i+1) == 0){
+				*e = (d+i);
+				break;
+			}
+		} 
+	}
+	return sum;
+}
